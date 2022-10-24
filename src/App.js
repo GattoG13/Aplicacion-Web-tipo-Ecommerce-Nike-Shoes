9edfb23adf7112e5b4fa4b40eca646da236290da
Gian Gatto <gattogiani1998@gmail.com>
2022-10-24 19:37:45 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
-import Body from "./assets/components/Body";
-import CartWidget from "./assets/components/CartWidget";
-import Footer from "./assets/components/Footer";
-import ItemListContainer from "./assets/components/ItemListContainer";
-import Navbar from "./assets/components/Navbar";
+import Body from "../src/assets/components/Body";
+import Footer from "../src/assets/components/Footer";
+import ItemListContainer from "../src/assets/components/ItemListContainer";
+import Navbar from "../src/assets/components/Navbar";
 import "./assets/css/App.css";
 
 // Father - functions go in here
-export default function Pepe() {
+function Pepe() {
   return (
     <>
-      <div className="Navbar">
-        <Navbar color={"#000000"} />
-        <CartWidget />
-      </div>
-      <div className="ItemList">
-        <ItemListContainer greeting={true} />
-      </div>
-      <div className="Body">
-        <Body />
-      </div>
-      <div className="Footer">
-        <Footer />
-      </div>
+      <Navbar color={"#000000"} />
+      <ItemListContainer greeting={true} />
+      <Body />
+      <Footer />
     </>
   );
 }
+
+export default Pepe;
